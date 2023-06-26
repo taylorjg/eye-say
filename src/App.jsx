1940c33ba13882ff4f48b9adeea96532839c1e4f
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-06-26 12:27:39 +0100

    Support manual deploy to gh-pages and add version info

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import ParentSize from "@visx/responsive/lib/components/ParentSize";
 
 import { EyeSayWordCloud } from "./EyeSayWordCloud";
+import { Version } from "./Version";
 import { StyledApp } from "./App.styles";
 
 export const App = () => {
@@ -9,6 +10,7 @@ export const App = () => {
       <ParentSize>
         {({ width, height }) => <EyeSayWordCloud width={width} height={height} />}
       </ParentSize>
+      <Version />
     </StyledApp>
   );
 };
---
+++ src/Version.jsx
@@ -0,0 +1,7 @@
+import packageJson from "../package.json";
+
+import { StyledVersion } from "./Version.styles";
+
+export const Version = () => {
+  return <StyledVersion>version: {packageJson.version}</StyledVersion>;
+};
